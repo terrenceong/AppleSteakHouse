85acf8d0fb37ab99a6afcf6ac6196cd3b13ebfde
terrenceong <terrenceongg@gmail.com>
2021-11-09 21:30:56 +0800

    bug fix

--- doc/package-search-index.js
+++ doc/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"invoice"},{"l":"reservation"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"invoice"},{"l":"menu"},{"l":"order"},{"l":"reservation"},{"l":"RRPSS"},{"l":"Test"}];updateSearchResults();
